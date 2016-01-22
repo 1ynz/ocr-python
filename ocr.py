07f003b2f1ea761a913cbbca73cce077a2627b0c
lin <lin@pc16.pc16.mis.nkfust.edu.tw>
2016-01-23 00:20:44 +0800

    update

--- ocr.py
+++ ocr.py
@@ -1,10 +1,13 @@
 import pyocr
 import pyocr.builders
+from PIL import Image
 
 #init OCR
 
+tool = pyocr.get_available_tools()[0]
+lang = tool.get_available_languages()[0]
+print(lang)
+img = Image.open('uccu.jpg')
 
+ans = tool.image_to_string(img, lang=lang, builder=pyocr.builders.TextBuilder())
 
-tools = pyocr.get_available_tools()[:]
-
-print(tools)
